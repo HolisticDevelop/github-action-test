4e28fdd6aac0f60aeb3de82ac819e77400caee0a
HolisticDev <holisticxsoft@gmail.com>
2024-03-04 09:43:25 -0500

    Initial commit

--- src/test/calc.test.ts
+++ src/test/calc.test.ts
@@ -2,6 +2,6 @@ import {add} from "../main/calc";
 
 describe('calculator', () => {
     it('should add two numbers', function () {
-        expect(add(1,2)).toEqual(3);
+        expect(add(1,3)).toEqual(4);
     });
 })
